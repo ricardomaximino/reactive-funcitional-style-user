862b3a4715bd4e4de13ae5bea1c062b88b89ac96
Ricardo Maximino Gonçalves de Moraes <rmgonsalves@vectoritcgroup.com>
2019-03-31 16:02:16 +0200

    working fine

--- src/main/java/com/brasajava/routerfunctionstyle/domain/entity/Person.java
+++ src/main/java/com/brasajava/routerfunctionstyle/domain/entity/Person.java
@@ -2,21 +2,17 @@ package com.brasajava.routerfunctionstyle.domain.entity;
 
 import java.time.LocalDate;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.domain.AbstractAggregateRoot;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.brasajava.routerfunctionstyle.message.model.Event;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Document
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class Person extends AbstractAggregateRoot<Person> {
 
-  @Id private String id;
   private String name;
   private String lastname;
   private LocalDate birthday;
